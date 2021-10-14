7b9888944cd8c03449bae4b88b22b7464ca8c75d
hailanxyouknow <hailan@ualberta.ca>
2021-10-14 16:40:53 -0600

    .

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -2,35 +2,35 @@ Notify() {
     # Check environment variables
     if [ -z "${R}" ] && [ -z "${N}" ]; then
         echo "Expect R (room id) or N (room name) to be provided"
-        exit -1
+        exit 1
     fi
 
     if [ -z "${M}" ]; then
         echo "Expect M (message) to be provided"
-        exit -1
+        exit 1
     fi
 
     if [ -z "${T}" ]; then
         echo "Expect T (Webex Token) to be provided" 
-        exit -1
+        exit 1
     fi
 
     # Determine R (Room ID) if it is not defined already
     if [ -z "${R}" ]; then
         RESPONSE=$(curl -s https://webexapis.com/v1/rooms -X GET -H "Authorization: Bearer $T" )
-        HAS_ERROR=$( echo $RESPONSE | jq '.errors' )
+        HAS_ERROR=$( echo "$RESPONSE" | jq '.errors' )
         if [ "$HAS_ERROR" != "null" ]; then
             echo "ERROR $HAS_ERROR"
-            exit -1;
+            exit 1;
         fi
         
         ROOMS=( $( echo $RESPONSE | jq -c '.items[] | select(.title==env.N)'))
         if [ "${#ROOMS[@]}" != 1 ]; then
             echo "ERROR: Cannot determine Webex Room ID"
             echo "=== ${#ROOMS[@]} rooms found ==="
-            echo $RESPONSE | jq '.items[] | select(.title==env.N)'
+            echo "$RESPONSE" | jq '.items[] | select(.title==env.N)'
         else
-            R=$( echo $ROOMS | jq '.id' )
+            R=$( echo "$ROOMS" | jq '.id' )
             echo "Room ID found: $R"
         fi
     fi
@@ -39,10 +39,10 @@ Notify() {
     RESPONSE2=$( curl -s https://webexapis.com/v1/messages -X POST \
         -H "Authorization: Bearer $T" -H 'Content-Type: application/json' \
         -d "{\"roomId\": $R,\"markdown\": \"$M\"}" )
-    HAS_ERROR=$( echo $RESPONSE2 | jq '.errors' )
+    HAS_ERROR=$( echo "$RESPONSE2" | jq '.errors' )
     if [ "$HAS_ERROR" != "null" ]; then
         echo "ERROR $HAS_ERROR"
-        exit -1;
+        exit 1;
     fi
     echo "Message sent: ${M}"
 }
